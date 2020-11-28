cbee22a9803dea5bd5a5dfc15954b346a6eca682
armagedon <abdo.vic@hotmail.com>
2020-11-28 23:46:42 +0100

    performance issues fixed

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,23 +1,37 @@
 import React from "react"
 import Title from "../Title"
 import testimonials from "../../constants/testimonials"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 import logo from "../../images/nat-8.jpg"
 import logoo from "../../images/nat-9.jpg"
-import video from "../../images/video.mp4"
-import videoo from "../../images/video.webm"
+
 
 const FeaturedTours = () => {
+  const image = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "nat-4.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  const backgroundFluidImageStack = [
+    image.image.childImageSharp.fluid,
+    `linear-gradient(to right bottom, #f6cd5f, #e3cf5b, #ced25a, #b8d35d, #a1d463, #7dd577, #54d48d, #00d1a4, #00c9ca, #00beeb, #00aeff, #2998ff)`,
+  ].reverse()
   return (
     <>
       <Title title="We make kids genuinely happy" />
       <section className="section-stories">
-        <div className="bg-video">
-          <video className="bg-video__content" autoPlay muted loop>
-            <source src={video} type="video/mp4" />
-            <source src={videoo} type="video/webm" />
-            Your browser is not supported!
-          </video>
-        </div>
+
+      <BackgroundImage
+          fluid={backgroundFluidImageStack}
+          className="bg-img"
+        ></BackgroundImage>
         {testimonials.map((item, index) => {
           return (
             <div className="row" key={index + 1}>
